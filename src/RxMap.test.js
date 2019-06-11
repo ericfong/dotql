1f6db97534e1de177c2af57dcd8367cb53113b5a
Eric <ericff@gmail.com>
2019-06-11 11:24:01 +0800

    fix: test

--- src/RxMap.test.js
+++ src/RxMap.test.js
@@ -12,12 +12,24 @@ test('get, set, watch', async () => {
   const watcher1 = jest.fn()
   m.watch('b', watcher1)
   await delay()
-  expect(watcher1).toBeCalledTimes(1)
-  // return get result
-  expect(watcher1).toBeCalledWith(undefined)
+  // will not emit if no value set
+  expect(watcher1).toBeCalledTimes(0)
 
   m.set('b', 'B')
   await delay()
-  expect(watcher1).toBeCalledTimes(2)
+  expect(watcher1).toBeCalledTimes(1)
   expect(watcher1).toBeCalledWith('B')
+
+  const watcher2 = jest.fn()
+  m.watch('b', watcher2)
+  await delay()
+  // first-emit if has value
+  expect(watcher2).toBeCalledTimes(1)
+  expect(watcher2).toBeCalledWith('B')
+
+  m.set('b', 'B1')
+  expect(watcher1).toBeCalledTimes(2)
+  expect(watcher1).toBeCalledWith('B1')
+  expect(watcher2).toBeCalledTimes(2)
+  expect(watcher2).toBeCalledWith('B1')
 })
