9ada8e7e76fb96e8580cff482abffa46c2ff17c9
Eric <ericff@gmail.com>
2019-06-12 11:08:10 +0800

    fix: fetchJson

--- src/fetchJson.js
+++ src/fetchJson.js
@@ -1,3 +1,9 @@
+const createError = (msg, values) => {
+  const error = new Error(msg)
+  Object.assign(error, values)
+  return error
+}
+
 const fetchJson = (url, option = {}, fetchFunc = global.fetch) => {
   const headers = (option.headers = { Accept: 'application/json', ...option.headers })
   if (option.json) {
@@ -7,18 +13,22 @@ const fetchJson = (url, option = {}, fetchFunc = global.fetch) => {
   if (option.body) {
     option.method = option.method || 'POST'
   }
+  option.credentials = option.credentials || 'include'
+
   return fetchFunc(url, option).then(res => {
     const type = res.headers.get('Content-Type')
-    const promise = type.includes('/json') ? res.json() : type.includes('/text') ? res.text() : Promise.resolve(res)
 
-    if (res.ok) return promise
+    // response content is JSON
+    if (type.includes('/json')) {
+      const promise = res.json()
+      if (res.ok) return promise
+      return promise.then(data => {
+        throw createError(`${(data && data.message) || res.statusText}`, { response: res, data })
+      })
+    }
 
-    // Fail http status
-    return promise.then(data => {
-      const error = new Error(`${(data && data.message) || res.statusText}`)
-      Object.assign(error, { response: res, data })
-      throw error
-    })
+    if (res.ok) return res
+    throw createError(`${res.statusText}`, { response: res })
   })
 }
 
